03f30c243202886a0e4466610972e990153f6246
Sourav Singha <sourav291990@gmail.com>
2024-07-12 19:26:51 +0530

    Implement Integration Testing Project

---
+++ CleanArchitecture.IntegrationTests/CustomerControllerIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanArchitecture.IntegrationTests;
+
+public class CustomerControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
+{
+    private readonly HttpClient _httpClient;
+
+    public CustomerControllerIntegrationTests(TestingWebAppFactory<Program> factory) => _httpClient = factory.CreateClient();
+
+    [Fact]
+    public async Task Get_WhenCalled_ReturnsCustomerList()
+    {
+        var response = await _httpClient.GetAsync("api/Customer");
+
+        var responseAsString = await response.Content.ReadAsStringAsync();
+
+    }
+}
---
+++ CleanArchitecture.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace CleanArchitecture.IntegrationTests;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using CleanArchitecture.Persistence.DbContexts;
+
+public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program>
+    where TEntryPoint : Program
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<CustomerDbContext>));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<CustomerDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryCustomerTest");
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            using var scope = serviceProvider.CreateScope();
+            using (var appContext = scope.ServiceProvider.GetRequiredService<CustomerDbContext>())
+            {
+                try
+                {
+                    appContext.Database.EnsureCreated();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        });
+    }
+}
